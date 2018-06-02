9ca8d520882b259531f63ee5e77df675bee7458c
supernimbus <mariofm.mx@gmail.com>
2018-06-01 20:56:44 -0700

    checkpoint for otp_enc.c file. Will run unit tests on school flip server prior to continuing any further.

--- Assignment4/otp_enc.c
+++ Assignment4/otp_enc.c
@@ -1,22 +1,129 @@
+/*********************************************************************
+** Program Filename: otp_enc.c
+** Author: Mario Franco-Munoz
+** Due Date: 6/12/2018
+** Description:CS344 Assignment 4:
+**  
+*********************************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
+//function prototypes
+void error(const char *);
+void sendMessage(char*, char*, int);
 
-int main(int argc, char *argv[])
-{
+
+
+
+
+
+
+void sendMessage(char* ptextFileName, char* keyFileName, char* port) {
+	FILE *messageFile;
+	FILE *keyFile;
+	char *message;
+	char *cypher;
+	int msgLen;
+	int cypherLen;
+	size_t buffersize = 0;
+	int ascii;
+	int portRead = atoi(port); 	
+
+
+	//first check to make sure that we are getting actual data and not null arguments			
+	if (ptextFileName == NULL || key == NULL || port == NULL) {
+		fprintf(stderr, "Error processing data.\n");
+		exit(1);	
+	}
+
+	//open the text and cypher files
+	messageFile	= open(ptextFileName, "r");
+    keyFile = open(keyFileName, "r");
+
+    msgLen = getline(&message, &buffersize, messageFile);
+    cypherLen = getline(&cypher, &buffersize, keyFile);
+
+    //check to make sure that the files are of the same length
+    if (msgLen != cypherLen) {
+    	if (cypherLen < msgLen) {
+    		fprintf(stderr, "Error: key '%s' is too short\n", keyFileName);
+    	}
+    	else {
+    		fprintf(stderr, "Error: key '%s' is too long\n", keyFileName);
+    	}
+
+    	//free memory and close files upon forceful exit	
+    	free(message);
+    	free(cypher);
+    	close(messageFile);
+    	close(keyFile);	
+
+    	exit(1);	
+    }
+
+    //validate the message: (should only contain capital letters and spaces)
+    for (int i = 0; i < msgLen; i++) {
+    	ascii = (int)message[i]; 
+    	if ( (ascii < 65 && ascii != 32) || (ascii > 90  && ascii != 32) ) {
+    		fprintf(stderr, "otp_enc error: input contains bad characters");
+    	}
+    }	
+
+    //free memory and close files	
+	free(message);
+    free(cypher);
+    close(messageFile);
+    close(keyFile);	
+
+}
+
+// Error function used for reporting issues
+void error(const char *msg) { 
+	fprintf(stderr, "Error Message: %s\n", strerror(errorno)); 
+	exit(1); 
+} 
+
+int main(int argc, char *argv[]) {
+	char* cypherText = NULL;	
+
+
+	//make sure that three arguments are being passed (plaintext, key, port) - four including the program itself	
+	if (argc != 4) {
+		error("Incorrect number of arguments passed to function.");	
+	}
+	else {
+		sendMessage(argv[1], argv[2], argv[3])	
+
+	}	
+
+
+
+
+
+
+
+
+/*
+		        
 	int socketFD, portNumber, charsWritten, charsRead;
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
 	char buffer[256];
     
-	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+	if (argc < 3) { 
+		fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
+		exit(0); 
+	} // Check usage & args
+
 
 	// Set up the server address struct
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
@@ -54,5 +161,7 @@ int main(int argc, char *argv[])
 	printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
 
 	close(socketFD); // Close the socket
+
+	*/
 	return 0;
 }
